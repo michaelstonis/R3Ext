000e50229fcb132fea6c75fce16c1d83e9131f1b
Michael Stonis <120685+michaelstonis@users.noreply.github.com>
2025-11-14 13:10:41 -0600

    feat: add CombineLatestValuesAreAllTrue operator

--- R3Ext/ReactivePortedExtensions.cs
+++ R3Ext/ReactivePortedExtensions.cs
@@ -523,6 +523,24 @@ public static class ReactivePortedExtensions
     }
 
     /// <summary>
+    /// Returns true when all latest values are true across the provided boolean observables.
+    /// </summary>
+    public static Observable<bool> CombineLatestValuesAreAllTrue(this System.Collections.Generic.IEnumerable<Observable<bool>> sources)
+    {
+        if (sources is null) throw new ArgumentNullException(nameof(sources));
+        var list = sources as System.Collections.Generic.IList<Observable<bool>> ?? new System.Collections.Generic.List<Observable<bool>>(sources);
+        return Observable.CombineLatest(list).Select(values =>
+        {
+            var all = true;
+            for (int i = 0; i < values.Length; i++)
+            {
+                if (!values[i]) { all = false; break; }
+            }
+            return all;
+        });
+    }
+
+    /// <summary>
     /// Ignore errors from the source and complete instead.
     /// </summary>
     public static Observable<T> CatchIgnore<T>(this Observable<T> source)
