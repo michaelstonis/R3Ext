af2dc0be2d6da13a3cc19ef0daa41c5d18cc93f4
Michael Stonis <120685+michaelstonis@users.noreply.github.com>
2025-11-19 16:51:23 -0600

    feat: Implement SourceCache with core cache infrastructure
    - Add ChangeSet<TObject, TKey> implementation
    - Add ISourceCache<TObject, TKey> interface
    - Add ICacheUpdater<TObject, TKey> interface for batch operations
    - Add SourceCache<TObject, TKey> implementation with:
      - Thread-safe dictionary-based storage
      - R3 Subject for change notifications
      - Edit() for batch updates
      - AddOrUpdate/Remove/Clear operations
      - Lookup for optional value retrieval
      - Connect() for observable subscription

---
+++ R3.DynamicData/Cache/ISourceCache.cs
@@ -0,0 +1,82 @@
+// Copyright (c) 2025 Michael Stonis. All rights reserved.
+// Port of DynamicData to R3.
+
+namespace R3.DynamicData.Cache;
+
+/// <summary>
+/// An editable cache that exposes an observable change set for tracking modifications.
+/// </summary>
+/// <typeparam name="TObject">The type of the object.</typeparam>
+/// <typeparam name="TKey">The type of the key.</typeparam>
+public interface ISourceCache<TObject, TKey> : IDisposable
+    where TKey : notnull
+{
+    /// <summary>
+    /// Gets the total count of cached items.
+    /// </summary>
+    int Count { get; }
+
+    /// <summary>
+    /// Gets all keys currently in the cache.
+    /// </summary>
+    IEnumerable<TKey> Keys { get; }
+
+    /// <summary>
+    /// Gets all items currently in the cache.
+    /// </summary>
+    IEnumerable<TObject> Items { get; }
+
+    /// <summary>
+    /// Gets all key-value pairs currently in the cache.
+    /// </summary>
+    IEnumerable<KeyValuePair<TObject, TKey>> KeyValues { get; }
+
+    /// <summary>
+    /// Looks up the value for the specified key.
+    /// </summary>
+    /// <param name="key">The key.</param>
+    /// <returns>The value if found, otherwise an empty optional.</returns>
+    Kernel.Optional<TObject> Lookup(TKey key);
+
+    /// <summary>
+    /// Connects to the cache and observes changes.
+    /// </summary>
+    /// <returns>An observable of change sets.</returns>
+    Observable<IChangeSet<TObject, TKey>> Connect();
+
+    /// <summary>
+    /// Connects to the cache and observes changes, applying an optional predicate filter.
+    /// </summary>
+    /// <param name="predicate">Optional predicate to filter items.</param>
+    /// <returns>An observable of change sets.</returns>
+    Observable<IChangeSet<TObject, TKey>> Connect(Func<TObject, bool>? predicate);
+
+    /// <summary>
+    /// Executes a batch update operation.
+    /// </summary>
+    /// <param name="updateAction">The update action.</param>
+    void Edit(Action<ICacheUpdater<TObject, TKey>> updateAction);
+
+    /// <summary>
+    /// Adds or updates the item with the specified key.
+    /// </summary>
+    /// <param name="item">The item to add or update.</param>
+    void AddOrUpdate(TObject item);
+
+    /// <summary>
+    /// Removes the item with the specified key.
+    /// </summary>
+    /// <param name="key">The key.</param>
+    void Remove(TKey key);
+
+    /// <summary>
+    /// Removes multiple items with the specified keys.
+    /// </summary>
+    /// <param name="keys">The keys to remove.</param>
+    void Remove(IEnumerable<TKey> keys);
+
+    /// <summary>
+    /// Clears all items from the cache.
+    /// </summary>
+    void Clear();
+}
---
+++ R3.DynamicData/Cache/SourceCache.cs
@@ -0,0 +1,287 @@
+// Copyright (c) 2025 Michael Stonis. All rights reserved.
+// Port of DynamicData to R3.
+
+namespace R3.DynamicData.Cache;
+
+/// <summary>
+/// An observable cache that tracks changes to items with unique keys.
+/// </summary>
+/// <typeparam name="TObject">The type of the object.</typeparam>
+/// <typeparam name="TKey">The type of the key.</typeparam>
+public sealed class SourceCache<TObject, TKey> : ISourceCache<TObject, TKey>
+    where TKey : notnull
+{
+    private readonly Func<TObject, TKey> _keySelector;
+    private readonly Dictionary<TKey, TObject> _data;
+    private readonly Subject<IChangeSet<TObject, TKey>> _changes;
+    private readonly object _locker = new();
+    private bool _isDisposed;
+
+    /// <inheritdoc/>
+    public int Count
+    {
+        get
+        {
+            lock (_locker)
+            {
+                return _data.Count;
+            }
+        }
+    }
+
+    /// <inheritdoc/>
+    public IEnumerable<TKey> Keys
+    {
+        get
+        {
+            lock (_locker)
+            {
+                return _data.Keys.ToList();
+            }
+        }
+    }
+
+    /// <inheritdoc/>
+    public IEnumerable<TObject> Items
+    {
+        get
+        {
+            lock (_locker)
+            {
+                return _data.Values.ToList();
+            }
+        }
+    }
+
+    /// <inheritdoc/>
+    public IEnumerable<KeyValuePair<TObject, TKey>> KeyValues
+    {
+        get
+        {
+            lock (_locker)
+            {
+                return _data.Select(kvp => new KeyValuePair<TObject, TKey>(kvp.Value, kvp.Key)).ToList();
+            }
+        }
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SourceCache{TObject, TKey}"/> class.
+    /// </summary>
+    /// <param name="keySelector">The function to extract the key from an object.</param>
+    public SourceCache(Func<TObject, TKey> keySelector)
+    {
+        _keySelector = keySelector ?? throw new ArgumentNullException(nameof(keySelector));
+        _data = new Dictionary<TKey, TObject>();
+        _changes = new Subject<IChangeSet<TObject, TKey>>();
+    }
+
+    /// <inheritdoc/>
+    public Kernel.Optional<TObject> Lookup(TKey key)
+    {
+        lock (_locker)
+        {
+            return _data.TryGetValue(key, out var value)
+                ? Kernel.Optional<TObject>.Some(value)
+                : Kernel.Optional<TObject>.None;
+        }
+    }
+
+    /// <inheritdoc/>
+    public Observable<IChangeSet<TObject, TKey>> Connect()
+    {
+        return _changes.AsObservable();
+    }
+
+    /// <inheritdoc/>
+    public Observable<IChangeSet<TObject, TKey>> Connect(Func<TObject, bool>? predicate)
+    {
+        if (predicate == null)
+        {
+            return Connect();
+        }
+
+        return _changes.AsObservable().Select(changes =>
+        {
+            var filtered = new ChangeSet<TObject, TKey>();
+            foreach (var change in changes)
+            {
+                if (predicate(change.Current))
+                {
+                    filtered.Add(change);
+                }
+            }
+
+            return (IChangeSet<TObject, TKey>)filtered;
+        });
+    }
+
+    /// <inheritdoc/>
+    public void Edit(Action<ICacheUpdater<TObject, TKey>> updateAction)
+    {
+        if (updateAction == null)
+        {
+            throw new ArgumentNullException(nameof(updateAction));
+        }
+
+        lock (_locker)
+        {
+            var updater = new CacheUpdater(this);
+            updateAction(updater);
+            PublishChanges(updater.GetChanges());
+        }
+    }
+
+    /// <inheritdoc/>
+    public void AddOrUpdate(TObject item)
+    {
+        Edit(updater => updater.AddOrUpdate(item));
+    }
+
+    /// <inheritdoc/>
+    public void Remove(TKey key)
+    {
+        Edit(updater => updater.Remove(key));
+    }
+
+    /// <inheritdoc/>
+    public void Remove(IEnumerable<TKey> keys)
+    {
+        Edit(updater => updater.Remove(keys));
+    }
+
+    /// <inheritdoc/>
+    public void Clear()
+    {
+        Edit(updater => updater.Clear());
+    }
+
+    /// <inheritdoc/>
+    public void Dispose()
+    {
+        if (_isDisposed)
+        {
+            return;
+        }
+
+        _isDisposed = true;
+        _changes.Dispose();
+    }
+
+    private void PublishChanges(ChangeSet<TObject, TKey> changes)
+    {
+        if (changes.Count > 0)
+        {
+            _changes.OnNext(changes);
+        }
+    }
+
+    private sealed class CacheUpdater : ICacheUpdater<TObject, TKey>
+    {
+        private readonly SourceCache<TObject, TKey> _cache;
+        private readonly ChangeSet<TObject, TKey> _changes;
+
+        public CacheUpdater(SourceCache<TObject, TKey> cache)
+        {
+            _cache = cache;
+            _changes = new ChangeSet<TObject, TKey>();
+        }
+
+        public int Count => _cache._data.Count;
+
+        public IEnumerable<TKey> Keys => _cache._data.Keys;
+
+        public IEnumerable<TObject> Items => _cache._data.Values;
+
+        public Kernel.Optional<TObject> Lookup(TKey key)
+        {
+            return _cache._data.TryGetValue(key, out var value)
+                ? Kernel.Optional<TObject>.Some(value)
+                : Kernel.Optional<TObject>.None;
+        }
+
+        public void AddOrUpdate(TObject item)
+        {
+            var key = _cache._keySelector(item);
+            var isUpdate = _cache._data.TryGetValue(key, out var previous);
+
+            _cache._data[key] = item;
+
+            var change = isUpdate
+                ? new Change<TObject, TKey>(Kernel.ChangeReason.Update, key, item, previous!)
+                : new Change<TObject, TKey>(Kernel.ChangeReason.Add, key, item);
+
+            _changes.Add(change);
+        }
+
+        public void AddOrUpdate(IEnumerable<TObject> items)
+        {
+            foreach (var item in items)
+            {
+                AddOrUpdate(item);
+            }
+        }
+
+        public void Remove(TKey key)
+        {
+            if (_cache._data.TryGetValue(key, out var value))
+            {
+                _cache._data.Remove(key);
+                _changes.Add(new Change<TObject, TKey>(Kernel.ChangeReason.Remove, key, value, value));
+            }
+        }
+
+        public void Remove(IEnumerable<TKey> keys)
+        {
+            foreach (var key in keys)
+            {
+                Remove(key);
+            }
+        }
+
+        public void RemoveKeys(Func<TKey, bool> predicate)
+        {
+            var keysToRemove = _cache._data.Keys.Where(predicate).ToList();
+            Remove(keysToRemove);
+        }
+
+        public void RemoveItems(Func<TObject, bool> predicate)
+        {
+            var keysToRemove = _cache._data
+                .Where(kvp => predicate(kvp.Value))
+                .Select(kvp => kvp.Key)
+                .ToList();
+            Remove(keysToRemove);
+        }
+
+        public void Clear()
+        {
+            var keys = _cache._data.Keys.ToList();
+            foreach (var key in keys)
+            {
+                var value = _cache._data[key];
+                _changes.Add(new Change<TObject, TKey>(Kernel.ChangeReason.Remove, key, value, value));
+            }
+
+            _cache._data.Clear();
+        }
+
+        public void Refresh(TKey key)
+        {
+            if (_cache._data.TryGetValue(key, out var value))
+            {
+                _changes.Add(new Change<TObject, TKey>(Kernel.ChangeReason.Refresh, key, value));
+            }
+        }
+
+        public void Refresh(IEnumerable<TKey> keys)
+        {
+            foreach (var key in keys)
+            {
+                Refresh(key);
+            }
+        }
+
+        public ChangeSet<TObject, TKey> GetChanges() => _changes;
+    }
+}
